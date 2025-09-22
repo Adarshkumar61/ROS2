a2c0e21955136f35d4bfaf002737f5eafbd3d9c2
adarshkumar61 <adarshb2k61@gmail.com>
2025-09-22 09:31:21 +0530

    code updated

--- my_robot_pkg/my_robot_pkg/listener.py
+++ my_robot_pkg/my_robot_pkg/listener.py
@@ -5,3 +5,20 @@ from std_msgs.msg import String
 class Subscriber(Node):
     def __init__(self):
         super().__init__('Publisher')
+        self.subscriptions(
+            String,
+            'chatter',
+            self.listener_callback,
+            10
+        )
+        self.subscriptions
+    
+    def listener_callback(self, msg):
+        self.get_logger().info(f'I heard: {msg.data}')
+
+def main(args = None):
+    rclpy.init(args=args)
+    node = Subscriber()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
