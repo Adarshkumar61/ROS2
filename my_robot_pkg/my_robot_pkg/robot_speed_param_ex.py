fb6b9b6301be1379bdb7653b22da89ba62aeb671
adarshkumar61 <adarshb2k61@gmail.com>
2025-09-23 17:25:15 +0530

    updated

--- my_robot_pkg/my_robot_pkg/robot_speed_param_ex.py
+++ my_robot_pkg/my_robot_pkg/robot_speed_param_ex.py
@@ -6,3 +6,5 @@ class robot_speed(Node):
     def __init__(self):
         super().__init__('robot_speed')
         self.declare_parameter('robot_speed', 1.0)
+        speed = self.get_parameter('robot speed').get_parameter_value().integer_value
+        
