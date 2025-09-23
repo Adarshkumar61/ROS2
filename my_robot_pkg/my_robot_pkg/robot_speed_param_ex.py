cb2b5a1b3a500a6800777fb6ff275306d598af9b
adarshkumar61 <adarshb2k61@gmail.com>
2025-09-23 17:28:21 +0530

    updated

--- my_robot_pkg/my_robot_pkg/robot_speed_param_ex.py
+++ my_robot_pkg/my_robot_pkg/robot_speed_param_ex.py
@@ -5,6 +5,13 @@ from std_msgs.msg import String
 class robot_speed(Node):
     def __init__(self):
         super().__init__('robot_speed')
-        self.declare_parameter('robot_speed', 1.0)
+        self.declare_parameter('robot_speed', 123)
         speed = self.get_parameter('robot speed').get_parameter_value().integer_value
-        
+        self.get_logger().info(f'B2S current speed is: {speed}/h')
+
+def main(args = None):
+    rclpy.init(args=args)
+    node = robot_speed()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
