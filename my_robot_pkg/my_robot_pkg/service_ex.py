20abf7e106e7fb74919c7d1c0cb9e9a877730b86
adarshkumar61 <adarshb2k61@gmail.com>
2025-09-24 21:06:03 +0530

    created service

--- my_robot_pkg/my_robot_pkg/service_ex.py
+++ my_robot_pkg/my_robot_pkg/service_ex.py
@@ -1,3 +1,24 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.srv import AddTwoInts   # Standard service type
+
+class AddTwoIntsService(Node):
+    def __init__(self):
+        super().__init__('add_two_ints_service')
+        # Create service
+        self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
+        self.get_logger().info("Service 'add_two_ints' is ready 🚀")
+
+    def add_two_ints_callback(self, request, response):
+        response.sum = request.a + request.b
+        self.get_logger().info(f"Received request: {request.a} + {request.b} = {response.sum}")
+        return response
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = AddTwoIntsService()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
