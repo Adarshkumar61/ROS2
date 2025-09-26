de70202e382831c77d8454020c6421f744737564
adarshkumar61 <adarshb2k61@gmail.com>
2025-09-26 08:26:43 +0530

    updated

--- urdf_robot_visualize/launch/display.launch.py
+++ urdf_robot_visualize/launch/display.launch.py
@@ -2,3 +2,11 @@ import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    urdf_file = os.path.join(
+        get_package_share_directory('urdf_robot_visualize'),
+        'urdf',
+        'cylinder.urdf.xacro'
+    )
+
