97e626725ab428b8c5bb00a7d0ad788198eae366
adarshkumar61 <adarshb2k61@gmail.com>
2025-09-26 09:54:09 +0530

    updated

--- urdf_robot_visualize/launch/display.launch.py
+++ urdf_robot_visualize/launch/display.launch.py
@@ -1,29 +1,31 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    # Path to your URDF file
     urdf_file = os.path.join(
         get_package_share_directory('urdf_robot_visualize'),
         'urdf',
-        'cylinder.urdf.xacro'
+        'my_robot.urdf'
     )
 
     return LaunchDescription([
-        Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui'
-        ),
+        # Robot State Publisher (publishes TF from URDF)
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            parameters=[{'robot_description': open(urdf_file).read()}]
+            arguments=[urdf_file],
+            output='screen'
         ),
+
+        # RViz2 to visualize
         Node(
             package='rviz2',
             executable='rviz2',
-            name='rviz2'
-        )
+            name='rviz2',
+            output='screen'
+        ),
     ])
