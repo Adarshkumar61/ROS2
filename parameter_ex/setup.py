3a53c83f7342b7893810bf530940b260fa3a9938
adarshkumar61 <adarshb2k61@gmail.com>
2025-10-02 17:42:42 +0530

    updated

--- parameter_ex/setup.py
+++ parameter_ex/setup.py
@@ -24,6 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'robot_speed_parameter_change_in_real_time = parameter_ex.robot_speed_parameter_change_in_real_time:main',
         ],
     },
 )
